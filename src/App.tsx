bd8e3fd5d413d29d2e6269e42d45a4c6a99d9a50
repmacrypn <artik3267@gmail.com>
2023-07-11 18:57:08 +0300

    some styles changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { FC, ChangeEvent, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { TextInput } from '@mantine/core';
 import './App.css';
 import { ITask } from './interfaces';
 import { TodoTask } from './components/TodoTask';
-import { nanoid } from 'nanoid';
+import { ListCheck, Pencil } from 'tabler-icons-react';
 
 const App: FC = () => {
   const [todos, setTodos] = useState<ITask[]>([])
   const [taskValue, setTaskValue] = useState<string>('')
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    setTaskValue(e.target.value)
+    setTaskValue(e.currentTarget.value)
   }
 
   const addTaskOnClick = (): void => {
     const newTask = { id: nanoid(), todoName: taskValue, isDone: false }
-    setTodos([...todos, newTask])
-    setTaskValue('')
+    if (taskValue) {
+      setTodos([...todos, newTask])
+      setTaskValue('')
+    }
   }
 
   const removeTaskOnClick = (id: string): void => {
@@ -33,20 +37,30 @@ const App: FC = () => {
   })
 
   return (
-    <div className="App">
-      <header>
-        header
-      </header>
-      <main>
-        <input
-          onChange={handleChange}
-          value={taskValue}
-          type='text'
-          placeholder='Input new task...'
+    <div className="appWrapper">
+      <header className='headerContainer'>
+        <ListCheck
+          strokeWidth={1.6}
+          size={50}
+          color='white'
         />
-        <button onClick={addTaskOnClick}>
-          Add task
-        </button>
+        <div className='headerTitle'>
+          Todo List app
+        </div>
+      </header>
+      <main className='mainContainer'>
+        <div className='addTaskWrapper'>
+          <TextInput
+            value={taskValue}
+            onChange={handleChange}
+            placeholder="New task..."
+            radius="md"
+            icon={<Pencil />}
+          />
+          <button onClick={addTaskOnClick}>
+            Add task
+          </button>
+        </div>
         <div>
           {todosResult}
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MantineProvider } from '@mantine/core';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MantineProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
